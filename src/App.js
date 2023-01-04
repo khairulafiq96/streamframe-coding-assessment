b050875328ec8eeaabf6ed7771d914eed2d3805f
Afiq <k.afiq.azman@gmail.com>
2023-01-04 13:21:00 +0800

    reworked tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-        <h1 class="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+    <div class='bg-slate-700 h-screen w-screen'>
+      <div>
+        Hello world
+      </div>
     </div>
   );
 }
